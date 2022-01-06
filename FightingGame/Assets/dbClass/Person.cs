c84dc8722a6d5a7913797774c3984bc687f70485
Denf00 <andylhs00@gmail.com>
2022-01-06 11:37:52 +1000

    dbClass

---
+++ FightingGame/Assets/dbClass/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect2SQL3
+{
+    internal class Person
+    {
+        public String Name { get; set; }
+        public Double HP { get; set; }
+        public Double Damage { get; set; }
+        public Double MovementSpeed { get; set; }
+        public Double Cooldown { get; set; }
+        public String Type { get; set; }
+
+        public Person(string _name)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "andysandboxdbsrv.database.windows.net";
+                builder.UserID = "andyl123";
+                builder.Password = "Qwerty00!";
+                builder.InitialCatalog = "fightingsqldb";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    //Console.WriteLine("\nExtracting data...");
+                    //Console.WriteLine("=========================================\n");
+
+                    String sql = "SELECT * FROM FightingGame.Person WHERE Person_Name = '" + _name + "'";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Name = reader.GetString(0);
+                                HP = reader.GetDouble(1);
+                                Damage = reader.GetDouble(2);
+                                MovementSpeed = reader.GetDouble(5);
+                                Cooldown = reader.GetDouble(3);
+                                Type = reader.GetString(4);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+           
+        }
+
+        public void ShowState()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("HP: " + HP);
+            Console.WriteLine("Damage: " + Damage);
+            Console.WriteLine("Movement Speed: " + MovementSpeed);
+            Console.WriteLine("Cooldown: " + Cooldown);
+            Console.WriteLine("Type: " + Type);
+        }
+
+
+    }
+}
---
+++ FightingGame/Assets/dbClass/getList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect2SQL3
+{
+    public class getList
+    {
+        public static string[] getHero(){
+
+            List<string> list = new List<string>();
+
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "andysandboxdbsrv.database.windows.net";
+                builder.UserID = "andyl123";
+                builder.Password = "Qwerty00!";
+                builder.InitialCatalog = "fightingsqldb";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    //Console.WriteLine("\nExtracting data...");
+                    //Console.WriteLine("=========================================\n");
+
+                    String sql = "SELECT Person_Name FROM FightingGame.Person WHERE PersonType = 'Hero'";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                list.Add(reader.GetString(0));
+                            }
+                        }
+                    }
+                }
+
+                return list.ToArray();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+    }
+}
